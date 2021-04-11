7b62db9c70d4a34a82cb40094123120a10dca8db
shahnewaz601 <shahnewaz601@gmail.com>
2021-04-12 02:03:24 +0600

    simple update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Blogs from '../Blogs/Blogs';
+import Contact from '../Contact/Contact';
+import Doctors from '../Doctors/Doctors';
 import FeaturedService from '../FeaturedService/FeaturedService';
 import Header from '../Header/Header';
 import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
+import Testimonials from '../Testimonials/Testimonials';
 
 const Home = () => {
     return (
@@ -11,6 +15,10 @@ const Home = () => {
             <Services></Services>
             <FeaturedService></FeaturedService>
             <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
+            <Blogs></Blogs>
+            <Doctors></Doctors>
+            <Contact></Contact>
         </div>
     );
 };
