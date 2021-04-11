c1f31dc3765b66191a127717efd6542267da709c
shahnewaz601 <shahnewaz601@gmail.com>
2021-04-11 21:36:55 +0600

    simpl update

--- src/components/HeaderMain/HeaderMain.js
+++ src/components/HeaderMain/HeaderMain.js
@@ -7,7 +7,7 @@ const HeaderMain = () => {
             <div className="col-md-4 offset-md-1">
                 <h1 style={{color: '#3A4256'}}>Your New Smile <br/> Starts Here</h1>
                 <p style={{color: '#3A4256'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat reiciendis iste vel omnis quod voluptatem sequi repellendus quos unde sunt.</p>
-                <button type="button" class="btn btn-primary text-uppercase">get appointment</button>
+                <button type="button" className="btn btn-primary text-uppercase">get appointment</button>
             </div>
             <div className="col-md-6">
                 <img src={chair} alt="chair" className="img-fluid"/>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,30 +4,30 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <Link to="#" class="nav-link me-5 font-500">Home</Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link to="#" className="nav-link me-5 font-500">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="#" class="nav-link me-5 font-500">About</Link>
+                        <li className="nav-item">
+                            <Link to="#" className="nav-link me-5 font-500">About</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="#" class="nav-link me-5 font-500">Dental Services</Link>
+                        <li className="nav-item">
+                            <Link to="#" className="nav-link me-5 font-500">Dental Services</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="#" class="nav-link me-5 font-500 text-white">Reviews</Link>
+                        <li className="nav-item">
+                            <Link to="#" className="nav-link me-5 font-500 text-white">Reviews</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="#" class="nav-link me-5 font-500 text-white">Blog</Link>
+                        <li className="nav-item">
+                            <Link to="#" className="nav-link me-5 font-500 text-white">Blog</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="#" class="nav-link me-5 font-500 text-white">Contact Us</Link>
+                        <li className="nav-item">
+                            <Link to="#" className="nav-link me-5 font-500 text-white">Contact Us</Link>
                         </li>
                     </ul>
                 </div>    
