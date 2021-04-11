5e572af9b74e492ee0058b1d4baf3c4b0e389b9d
shahnewaz601 <shahnewaz601@gmail.com>
2021-04-12 00:10:33 +0600

    set services  and related data with style

--- src/components/HeaderMain/HeaderMain.js
+++ src/components/HeaderMain/HeaderMain.js
@@ -7,7 +7,7 @@ const HeaderMain = () => {
         <main style={{height: '600px'}} className="d-flex align-items-center">
             <div className="col-md-4 offset-md-1">
                 <h1 style={{color: '#203047'}}className="mb-4">Your New Smile <br/> Starts Here</h1>
-                <p style={{color: '#203047'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat reiciendis iste vel omnis quod voluptatem sequi repellendus quos unde sunt.</p>
+                <p className="text-secondary">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat reiciendis iste vel omnis quod voluptatem sequi repellendus quos unde sunt.</p>
                 <button type="button" className="btn btn-primary text-uppercase mt-4">get appointment</button>
             </div>
             <div className="col-md-6 headerImg">
---
+++ src/components/Services/Services.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import fluride from '../../images/fluoride.png';
+import cavity from '../../images/cavity.png';
+import whitening from '../../images/whitening.png';
+import ServiceDetail from '../ServiceDetail/ServiceDetail';
+
+const serviceData = [
+    {
+        id: 1,
+        name: 'Fluoride Treatment',
+        description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+        img: fluride
+    }, {
+        id: 2,
+        name: 'Cavity Filling',
+        description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+        img: cavity
+    }, {
+        id: 3,
+        name: 'Teath Whitening ',
+        description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+        img: whitening
+    }
+]
+
+const Services = () => {
+    return (
+        <section>
+           <div className="text-center mt-5">
+                <h5 style={{color: '#5FC7C7'}}>Our Services</h5>
+                <h2 style={{color: '#203047'}}>Services We Provide</h2>
+           </div>
+           <div className="d-flex justify-content-center">
+                <div className="w-75 row mt-5 pt-5">
+                    {
+                        serviceData.map(service => <ServiceDetail service={service} key={service.id}></ServiceDetail>)
+                    }
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Services;
